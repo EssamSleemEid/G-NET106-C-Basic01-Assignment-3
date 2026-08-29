972e065d988ae126b2590427e7c9adedaff5e1ff
Nongoogle <essamsleem9@gmail.com>
2026-08-29 23:39:45 +0300

    Question01

--- Program.cs
+++ Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question1
+            //1-  Given int pages = 464; and bool isAvailable = true;, print "You can borrow this book" only if pages is greater than 300 and isAvailable is true. Use the && operator. 
+
+            //int pages = 464;
+
+            //bool isAvailable = true;
+
+            //if (pages >300 && isAvailable)
+            //{
+            //    Console.WriteLine("you can borrow this book");
+            //}
+            #endregion
+
+
         }
     }
 }
