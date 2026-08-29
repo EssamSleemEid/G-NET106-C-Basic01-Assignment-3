ca64888d5b303aa5f9a3cdc00906514bf925d595
Nongoogle <essamsleem9@gmail.com>
2026-08-29 23:46:55 +0300

    Question02

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Second_Assignment
+﻿using System.Security.Cryptography;
+
+namespace Second_Assignment
 {
     internal class Program
     {
@@ -15,6 +17,27 @@
             //{
             //    Console.WriteLine("you can borrow this book");
             //}
+            #endregion
+
+            #region Question2
+            //2-  Given string title = "Refactoring";, write a switch statement that prints "Great choice!" if the title is "Clean Code", "Nice pick!" if it's "Refactoring", and "Never heard of it" for anything else. 
+
+            //string title = "Refactoring";
+
+            //switch (title)
+            //{
+
+            //    case "Clean Code":
+            //        Console.WriteLine("Great choice");
+            //        break;
+            //    case "Refactoring":
+            //        Console.WriteLine("Nice pick");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Never heard of it");
+            //        break;
+            //}
+
             #endregion
 
 
